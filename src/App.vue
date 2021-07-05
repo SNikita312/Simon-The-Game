6c483dc1b1afd9512bb3060a5211f22aa9f60beb
brave312guide <nikita.the.best312@gmail.com>
2021-07-05 05:07:47 +0300

    comit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Game/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Game from "./components/Game";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {Game}
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #6c98c7;
+  background: #333333;
+  min-height: 100vh;
 }
 </style>
